d9e4f82f6cf53a4b0d15852188144e63187c2ae6
Nikhil6765 <98408521+Nikhil6765@users.noreply.github.com>
2023-03-03 13:20:28 +0530

    Update getset.sol

--- getset.sol
+++ getset.sol
@@ -2,24 +2,47 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+pragma solidity ^0.8.0;
+
 contract MyContract {
-
-    uint num;
-    string myString;
+    string  myString;
+    uint  myUint;
+    bool  myBool;
     address myAddress;
-    bool myBool;
 
-    function get() public view returns( uint,string memory,address,bool) {
-        return ( num, myString, myAddress, myBool);
+    function setMyString(string memory _value) public {
+        myString = _value;
+        return myString;
     }
 
-    function set(uint _num, string memory _myString ,address _myAddress, bool _myBool ) public returns( uint,string memory,address,bool) { 
-        num = _num;
-        myString= _myString;
-        myAddress = _myAddress;
-        myBool= _myBool;
-        return (num, myString, myAddress, myBool);
+    function getMyString() public view returns (string memory) {
+        return myString;
+    }
+
+    function setMyUint(uint _value) public {
+        myUint = _value;
+        return myUint;
+    }
+
+    function getMyUint() public view returns (uint) {
+        return myUint;
+    }
+
+    function setMyBool(bool _value) public {
+        myBool = _value;
+        return myBool;
+    }
+
+    function getMyBool() public view returns (bool) {
+        return myBool;
+    }
+
+    function setMyAddress(address _value) public {
+        myAddress = _value;
+        return myAddress;
+    }
+
+    function getMyAddress() public view returns (address) {
+        return myAddress;
     }
 }
-
-    
